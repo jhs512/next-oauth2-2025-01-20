806060d6fdf34e1bcfa5346af64d260490bd55cb
jhs512 <jangka512@gmail.com>
2025-01-22 16:17:04 +0900

    work

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,20 +1,35 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { MessageCircle } from "lucide-react";
+import { useState } from "react";
 
 export default function Page() {
   const socialLoginForKakaoUrl = `http://localhost:8080/oauth2/authorization/kakao`;
   const redirectUrlAfterSocialLogin = "http://localhost:3000";
 
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <div className="flex-1 flex justify-center items-center">
-      <Button variant="outline" asChild>
-        <a
-          href={`${socialLoginForKakaoUrl}?redirectUrl=${redirectUrlAfterSocialLogin}`}
+      <div>
+        <div
+          className={`w-[100px] h-[100px] bg-red-500 on:bg-blue-500 ${
+            isClicked ? "on" : ""
+          }`}
+          onClick={() => setIsClicked(!isClicked)}
         >
-          <MessageCircle />
-          <span className="font-bold">카카오 로그인</span>
-        </a>
-      </Button>
+          {isClicked ? "clicked" : "not clicked"}
+        </div>
+        <Button variant="outline" asChild>
+          <a
+            href={`${socialLoginForKakaoUrl}?redirectUrl=${redirectUrlAfterSocialLogin}`}
+          >
+            <MessageCircle />
+            <span className="font-bold">카카오 로그인</span>
+          </a>
+        </Button>
+      </div>
     </div>
   );
 }
