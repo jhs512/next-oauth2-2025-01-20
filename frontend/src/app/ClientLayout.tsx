c9f0b17d0e3182bc538c500b0b6804601c3f939a
jhs512 <jangka512@gmail.com>
2025-01-22 23:35:06 +0900

    work

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -3,8 +3,10 @@
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import * as React from "react";
 
-import { Moon, Sun } from "lucide-react";
+import { Home, LogIn, LogOut, Moon, Settings, Sun, User } from "lucide-react";
 import { useTheme } from "next-themes";
+
+import { LoginMemberContext, useLoginMember } from "@/stores/auth/loginMember";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -14,6 +16,8 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 function ModeToggle() {
   const { setTheme } = useTheme();
@@ -45,6 +49,54 @@ function ModeToggle() {
 export function ClientLayout({
   children,
 }: React.ComponentProps<typeof NextThemesProvider>) {
+  const router = useRouter();
+
+  const {
+    setLoginMember,
+    isLogin,
+    loginMember,
+    removeLoginMember,
+    isLoginMemberPending,
+    isAdmin,
+    setNoLoginMember,
+  } = useLoginMember();
+
+  const loginMemberContextValue = {
+    loginMember,
+    setLoginMember,
+    removeLoginMember,
+    isLogin,
+    isLoginMemberPending,
+    isAdmin,
+    setNoLoginMember,
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoginMember({
+        id: 2,
+        createDate: "",
+        modifyDate: "",
+        nickname: "admin",
+      });
+    }, 1000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (isLoginMemberPending) {
+    return (
+      <div className="flex-1 flex justify-center items-center text-muted-foreground">
+        인증 정보 로딩중...
+      </div>
+    );
+  }
+
+  const logout = () => {
+    removeLoginMember();
+    router.replace("/");
+  };
+
   return (
     <NextThemesProvider
       attribute="class"
@@ -52,17 +104,63 @@ export function ClientLayout({
       enableSystem
       disableTransitionOnChange
     >
-      <header className="flex p-2 gap-4">
-        <Button variant="link" asChild>
-          <Link href="/">홈</Link>
-        </Button>
-        <div className="flex-grow"></div>
-        <ModeToggle />
-      </header>
-      <main className="flex-1 flex flex-col">{children}</main>
-      <footer className="p-2 flex justify-center">
-        <span>푸터</span>
-      </footer>
+      <LoginMemberContext value={loginMemberContextValue}>
+        <header className="flex p-2">
+          <Button variant="link" asChild>
+            <Link href="/">
+              <Home /> 홈
+            </Link>
+          </Button>
+          {isAdmin && (
+            <Button variant="link" asChild>
+              <Link href="/adm">
+                <Settings /> 관리자
+              </Link>
+            </Button>
+          )}
+          {isLogin && (
+            <Button variant="link" asChild>
+              <Link href="/member/me">
+                <User /> 내 정보
+              </Link>
+            </Button>
+          )}
+          {isLogin && (
+            <Button variant="link" onClick={logout}>
+              <LogOut /> 로그아웃
+            </Button>
+          )}
+          {!isLogin && (
+            <Button variant="link" asChild>
+              <Link href="/adm/member/login">
+                <LogIn /> 관리자 로그인
+              </Link>
+            </Button>
+          )}
+          <div className="flex-grow"></div>
+          <ModeToggle />
+        </header>
+        <main className="flex-1 flex flex-col">{children}</main>
+        <footer className="p-2 flex justify-center">
+          <Button variant="link" asChild>
+            <Link href="/adm">
+              <Settings /> 관리자
+            </Link>
+          </Button>
+
+          <Button variant="link" asChild>
+            <Link href="/adm/member/login">
+              <LogIn /> 관리자 로그인
+            </Link>
+          </Button>
+
+          <Button variant="link" asChild>
+            <Link href="/member/me">
+              <User /> 내 정보
+            </Link>
+          </Button>
+        </footer>
+      </LoginMemberContext>
     </NextThemesProvider>
   );
 }
